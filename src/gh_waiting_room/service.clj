d57bfeec3fadc003488179e9ba6d5a7630238352
Gabriel Horner <gabriel.horner@gmail.com>
2013-04-07 13:15:15 -0400

    first pass at showing a useful backlog of issues

--- src/gh_waiting_room/service.clj
+++ src/gh_waiting_room/service.clj
@@ -5,14 +5,56 @@
               [io.pedestal.service.http.route.definition :refer [defroutes]]
               [io.pedestal.service.interceptor :refer [defon-response]]
               [clostache.parser :refer [render-resource]]
+              [tentacles.issues :refer [my-issues]]
               [ring.util.response :as ring-resp]))
+
+(def db (atom {}))
+
+(defn- get-auth
+  []
+  (or (System/getenv "GITHUB_AUTH")
+      (throw (ex-info "Set $GITHUB_AUTH to fetch github issues" {}))))
+
+(defn- api-options
+  []
+  {:filter "all" :all-pages true :auth (get-auth)})
+
+;; TODO: make this customizable
+(defn- issue-filter
+  [issue]
+  (and (re-find #"github.com/cldwalker" (str (:html_url issue)))
+       (= [] (:labels issue))))
+
+(defn- fetch-gh-issues
+  []
+  (swap! db assoc :issues (my-issues (api-options))))
+
+(defn ->issue [issue]
+  {:name (str
+          (or (re-find #"[^/]+/[^/]+(?=/issues/\d+)" (:html_url issue))
+              (throw (ex-info "Failed to parse name from an issue" {:issue issue})))
+          "#"
+          (:number issue))
+   :url (:html_url issue)
+   :desc (str (re-find #".{0,100}\S" (:body issue))
+              (if (> (count (:body issue)) 100) "") " ...")
+   :user (or (get-in issue [:user :login])
+             (throw (ex-info "No user found for issue" {:issue issue})))
+   :created (or (re-find #"\d{4}-\d\d-\d\d"(:created_at issue))
+                (throw (ex-info "Failed to parse date from an issue" {:issue issue})))})
 
 (defn home-page
   [request]
+  (when-not (seq (:issues @db)) (fetch-gh-issues))
   (ring-resp/response
    (render-resource "public/index.mustache"
                     {:github-user (or (System/getenv "GITHUB_USER") "FIXME")
-                     :issues [{:name "cldwalker/debugger#71"} {:name "cldwalker/ripl#10"}]})))
+                     :issues (->> (:issues @db)
+                                  (filter issue-filter)
+                                  (map ->issue)
+                                  reverse
+                                  (map-indexed (fn [num elem]
+                                                 (assoc elem :position (inc num)))))})))
 
 (defon-response html-content-type
   [response]
